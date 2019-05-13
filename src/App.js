68324a74c144e4a017197de3df142a2d77a5663b
chastity duhaney <chastity_duhaney82@gmail.com>
2019-05-13 11:30:32 -0400

    added api for food recipes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from "./components/Form";
+const API_KEY = "4104dc75366581b4e5a9464490c0b270"
+
 
 class App extends Component {
-  getRecipe = (e) => {
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    console.log("Working!!!");
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=5`);
+    //console.log(recipeName);
+
+    const data = await api_call.json();
+    console.log(data);
 
   }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const Form = props => (
     <form onSubmit={props.getRecipe}>
-        <input type="text" />
+        <input type="text" name="recipeName" />
         <button>Search</button>
     </form>
 );
